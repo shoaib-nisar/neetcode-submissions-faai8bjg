6197b51859ef3cf21715d8189dc418bc267f97d5
Shoaib <166142808+shoaib-nisar@users.noreply.github.com>
2026-07-23 03:18:52 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+    
+    string str="";
+    for(auto it:s)
+    {
+        if(isalnum(it))
+        str+=tolower(it);
+    }
+    int l=0,r=str.size()-1;
+     while(l<=r)
+     {
+        if(str[l]!=str[r])
+        return false;
+        l++;
+        r--;
+     }   
+     return true;
+    }
+};
